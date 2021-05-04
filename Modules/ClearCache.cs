72489f8057c360971b6aff0c2af36d94f5486d34
Aaron Robinson <aaronrobin1234@gmail.com>
2021-05-03 19:03:07 -0500

    Add clear cache command

---
+++ Modules/ClearCache.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using RoR2Checker.Modules.Preconditions;
+
+namespace RoR2Checker.Modules
+{
+    public class ClearCacheModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("clearcache")]
+        [RequireOwner(Group = "Auth")]
+        [ThunderstoreModerator(Group = "Auth")]
+        public async Task ClearCache()
+        {
+            foreach (var file in Directory.GetFiles("Temp", "*").Concat(Directory.GetFiles("Cache", "*")))
+                File.Delete(file);
+
+            await ReplyAsync("Cleared cache directories");
+
+            return;
+        }
+    }
+}
